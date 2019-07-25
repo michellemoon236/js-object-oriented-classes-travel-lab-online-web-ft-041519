ba2998a69458f82144fd1d1ada804beb589f395a
michellemoon236 <michellemoon236@gmail.com>
2019-07-24 22:21:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = startDate
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let startYear = this.startDate.split(" ")[2]
+    return year - startYear
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+
+  }
+
+  estimatedTime(peak) {
+    if (peak === true) {
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
